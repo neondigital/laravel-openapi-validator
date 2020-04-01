654ec3ae5ff46edf89371e8d72b1e341f66a29d3
Alec <hello@itsalec.co.uk>
2020-04-01 10:44:34 +0100

    Init

---
+++ src/Providers/OpenApiValidatorProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NeonDigital\OpenApiValidator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenApiValidatorProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
---
+++ src/ValidatesWithOpenApi.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Neondigital\LaravelOpenapiValidator;
+
+trait ValidatesWithOpenApi
+{
+    
+}
